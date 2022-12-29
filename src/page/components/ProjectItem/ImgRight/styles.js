e5fc81a69a52de6fc4801c8b4bd8c22b8486c193
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-12-29 13:02:38 -0300

    Implementação do projeto "Pokédex"

--- src/page/components/ProjectItem/ImgRight/styles.js
+++ src/page/components/ProjectItem/ImgRight/styles.js
@@ -58,11 +58,12 @@ export const ImageContainer = styled.div`
         height: 18rem;
 
 
+        display: flex;
         align-items: center;
         justify-content: center;
 
         img {
-            width: auto;
+            width: 100%;
             height: auto;
         }
     }
--- src/page/sections/Projects/index.js
+++ src/page/sections/Projects/index.js
@@ -3,6 +3,8 @@ import { ProjectItemRight } from '../../components/ProjectItem/ImgRight'
 import { ProjectItemLeft } from '../../components/ProjectItem/ImgLeft'
 import { Container, SkillContainer, TitleContainer } from './styles'
 
+import PokedexImg from '../../../assets/projectarts/reactpokedex.png'
+
 const Projects = () => {
   return (
     <Container>
@@ -11,12 +13,14 @@ const Projects = () => {
       </TitleContainer>
       <SkillContainer>
         <ProjectItemRight
-          projectType="🥪 SMART SHOPPER"
-          project="Meat Cheese Bread"
-          description="Team-developed project completed while part of The Collab Lab Winter 2021 Cohort. A React PWA bootstrapped with Firestore, deployed to Netlify. The focus of this project was most importantly about collaboration and the inclusion of all voices through best practice workflows with weekly tickets, grooming, and feedback from Senior Devs."
-          href="/"
+          projectType="📟 InfoConSulta"
+          project="PokéDex"
+          description="Projeto desenvolvido com o principal objetivo de aprendizado em requisição de uma api remota, utilizando a função GET para a consulta de itens que projeto são pokémons. Outros métodos testados são de filtragem de busca pelos nomes dos pokémons e aprimoramento em css com aplicação do display grid, flex-box, webkit de aniações entre outros."
+          href="https://github.com/ulissesmarciano/projects/tree/main/pokedex"
           tec1="React.js"
-          img="https://kevin-davis.com/static/ccbb985979df477c76be7286f24fed95/64b17/mcb_screens7.jpg"
+          tec2="React Native"
+          tec3="Javascript"
+          img={PokedexImg}
         />
         <ProjectItemLeft 
           projectType="🥪 SMART SHOPPER"
