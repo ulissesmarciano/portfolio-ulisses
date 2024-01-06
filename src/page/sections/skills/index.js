8d9ea338522c99a365caa68fceedf903e59d98b9
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2024-01-05 23:00:49 -0300

    adicionando skills novas

--- src/page/sections/skills/index.js
+++ src/page/sections/skills/index.js
@@ -13,6 +13,10 @@ import HtmlIcon from '../../../assets/icons/html-5.svg'
 import JSIcon from '../../../assets/icons/javascript.svg'
 import NodeJsIcon from '../../../assets/icons/nodejs.svg'
 import TSIcon from '../../../assets/icons/typescript.svg'
+import DotNetIcon from '../../../assets/icons/dotnet.svg'
+import CSharpIcon from '../../../assets/icons/csharp.svg'
+
+
 
 
 const SkillsContent = () => {
@@ -22,16 +26,18 @@ const SkillsContent = () => {
         <h3 id='Habilidades'>Habilidades</h3>
       </TitleContainer>
         <SkillsContainer>
+          <Skill src={JSIcon} alt="Icone Javascript" title="Javascript"/>
+          <Skill src={TSIcon} alt="Icone Typescript" title="Typescript"/>
+          <Skill src={CSharpIcon} alt="Icone CSharp" title="CSharp"/>
           <Skill src={ReactIcon} alt="Icone React" title="React.js"/>
-          <Skill src={AndroidIcon} alt="Icone Android" title="Android Dev"/>
           <Skill src={AngularIcon} alt="Icone Angular" title="Angular.js"/>
-          <Skill src={AWSIcon} alt="Icone AWS" title="Cloud Computing"/>
+          <Skill src={AndroidIcon} alt="Icone Android" title="Android Dev"/>
+          <Skill src={DotNetIcon} alt="Icone DotNet" title=".NET"/>
           <Skill src={CSS3Icon} alt="Icone Css3" title="CSS3"/>
           <Skill src={GitIcon} alt="Icone Git" title="Git"/>
           <Skill src={HtmlIcon} alt="Icone Html" title="HTML5"/>
-          <Skill src={JSIcon} alt="Icone Javascript" title="Javascript"/>
           <Skill src={NodeJsIcon} alt="Icone Nodejs" title="Node.js"/>
-          <Skill src={TSIcon} alt="Icone Typescript" title="Typescript"/>
+          <Skill src={AWSIcon} alt="Icone AWS" title="Cloud Computing"/>
         </SkillsContainer>
     </div>
   )
