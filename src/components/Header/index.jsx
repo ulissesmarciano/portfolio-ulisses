502b18438ae298d02f157ea60575b0468078a55d
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-02 20:28:12 -0300

    Entrada e posicionamento dos itens de navegação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,32 +5,30 @@ import LinkItem from '../LinkItem';
 const Header = () => {
   return (
     <Container>
-      <nav>
-        <h1>Ulisses Marciano</h1>
-        <nav>
-          <ul>
-            <li>
-              <LinkItem
-               name="Sobre"
-              />
-            </li>
-            <li>
-              <LinkItem
-               name="Habilidades"
-              />
-            </li>
-            <li>
-              <LinkItem
-               name="Projetos"
-              />
-            </li>
-            <li>
-              <LinkItem
-               name="Contato"
-              />
-            </li>
-          </ul>
-        </nav>
+      <h1 className='portfolio-title'>Ulisses Marciano</h1>
+      <nav className='navigation-bar'>
+        <ul>
+          <li>
+            <LinkItem
+              name="Sobre"
+            />
+          </li>
+          <li>
+            <LinkItem
+              name="Habilidades"
+            />
+          </li>
+          <li>
+            <LinkItem
+              name="Projetos"
+            />
+          </li>
+          <li>
+            <LinkItem
+              name="Contato"
+            />
+          </li>
+        </ul>
       </nav>
     </Container>
   )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-    background-color: red;
+    margin: 0 auto;
+    padding: 1rem 0;
+    max-width: 64rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+   .portfolio-title {
+        font-size: 2rem;
+        font-weight: 800;
+        flex: 1;
+
+        background-color: red;
+   }
+   
+   .navigation-bar {
+        flex: 1;
+   }
+
+   .navigation-bar ul {
+        background-color: yellow;
+        display: flex;
+        justify-content: space-between;
+   }
 `
