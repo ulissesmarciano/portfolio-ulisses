ac09c10d09bb2a972c9acd2606b8da0a8e01389f
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-02 19:27:37 -0300

    Atualização da folha de estilos global do projeto

--- src/style/global.js
+++ src/style/global.js
@@ -6,41 +6,10 @@ export const Globalstyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        scroll-behavior: smooth;
-        
-    }
-
-    body {
-        padding: 2rem 2rem 0 2rem;
-        font-family: Roboto, Arial, sans-serif;
-        background-color: #FFFFFF;
-
-        display: flex;
-        flex-direction: column;
-    }
-
-    h1 {
-        color: #000000;
-        font-size: 2rem;
-        font-weight: 800;
-    }
-
-    h3 {
-        font-size: 1.75rem;
-        margin-bottom: 3rem;
-    }
-
-    a{
+        font-family: Roboto;
+        list-style: none;
         text-decoration: none;
-        color: #000000;
+        //scroll-behavior: smooth;
     }
-
-    p{
-        font-size: 1.2rem;
-        font-weight: 400;
-        margin-bottom: 1rem;
-        line-height: 1.5rem;
-    }
-
 `
 
