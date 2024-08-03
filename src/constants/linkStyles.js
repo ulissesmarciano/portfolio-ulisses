9c8649aa5b2d14c1c4daa75f885ad925e08f02da
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-03 16:24:07 -0300

    Adição de nova variante na para novos links renderizados.

--- src/constants/linkStyles.js
+++ src/constants/linkStyles.js
@@ -35,4 +35,43 @@ export const linkStyles = {
             background-color: #000;
         }
     `,
+
+    greetingsContactLink: css`
+        padding: 0.8rem 1.5rem;
+        
+        position: relative;
+        overflow: hidden;
+        
+        font-weight: 500;
+        border: 2px solid #000;
+        border-radius: 1rem;
+
+        display: flex;
+        align-items: center;
+        
+        &:after {
+            content: '';
+            
+            position: absolute;
+            left: 0;
+            bottom: 0;
+
+            width: 0;
+            height: 100%;
+
+            background-color: #05a1ad;
+            
+            z-index: -1;
+
+            transition: width 200ms ease;
+        }
+
+        &:hover {
+            &:after {
+                width: 100%;
+            }
+            
+            color: #fff;
+        }
+    `,
 }
