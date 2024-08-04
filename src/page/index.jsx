a895581c3791a74201a4fbbe6b80471374a52694
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-04 10:07:00 -0300

    Migração de novo componente e limpeza na folha de estilos

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
 import { Container } from './styles'
 import Header from '../components/Header'
-import LinkItem from '../components/LinkItem'
+import { AboutMeSection } from '../components/AboutMeSection'
+import { GreetingsSection } from '../components/GreetingSection'
 
 const PortfolioSection = () => {
   return (
     <Container>
       <Header />
       <main className='content-section'>
-        <section className='greeting-screen'>
-          <h3>Olá, me chamo Ulisses <span className='waving-hand'>&#128075;</span></h3>
-          <p className='summary-sentence'>Sou um <span>desenvolvedor full-stack</span>, com treinamento certificado e também <span>desenvolvo aplicativos Android</span>.</p>
-          <div className='contact-screen'>
-            <p>Conheça um pouco do meu trabalho e fique à vontade para entrar em contato.</p>
-            <div className='contact-link-screen'> 
-              <LinkItem 
-                name="LinkedIn"
-                variant="greetingsContactLink"
-              />
-              <LinkItem 
-                name="Github"
-                variant="greetingsContactLink"
-              />
-              <LinkItem 
-                name="Dribbble"
-                variant="greetingsContactLink"
-              />
-            </div>
-          </div>
-        </section>
-        <section className='about-me-section'>
-          teste
-        </section>
+        <GreetingsSection />
+        <AboutMeSection />
       </main>
     </Container>
   )
