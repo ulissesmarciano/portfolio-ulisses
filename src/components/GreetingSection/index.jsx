293117488991e25a68b4f28eadaa75ae8a58ea41
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-04 10:05:52 -0300

    Adição da folha de estilos e migração do compomente criado

--- src/components/GreetingSection/index.jsx
+++ src/components/GreetingSection/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Container } from './styles';
+import LinkItem from '../LinkItem';
 
 export const GreetingsSection = () => {
   return (
-    <div>GreetingsSection</div>
-  )
-}
+    <Container>
+        <h3 className='hello-section'>Olá, me chamo Ulisses <span className='waving-hand'>&#128075;</span></h3>
+        <p className='summary-sentence'>Sou um <span>desenvolvedor full-stack</span>, com treinamento certificado e também <span>desenvolvo aplicativos Android</span>.</p>
+        <div className='contact-screen'>
+            <p>Conheça um pouco do meu trabalho e fique à vontade para entrar em contato.</p>
+            <div className='contact-link-screen'> 
+                <LinkItem 
+                    name="LinkedIn"
+                    variant="greetingsContactLink"
+                />
+                <LinkItem 
+                    name="Github"
+                    variant="greetingsContactLink"
+                />
+                <LinkItem 
+                    name="Dribbble"
+                    variant="greetingsContactLink"
+                />
+            </div>
+        </div>
+    </Container>
+  );
+};
---
+++ src/components/GreetingSection/styles.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    padding-bottom: 8rem;
+    height: calc(100vh - 98.78px);
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    .hello-section {
+        margin-bottom: 2.2rem;
+
+        display: flex;
+        align-items: flex-start;
+        gap: 1rem;
+
+        font-size: 2.625rem;
+    }
+
+    .hello-section .waving-hand {
+        font-size: 2rem;
+        display: inline-block;
+        animation: wave 2s infinite;
+        transform-origin: 70% 70%;
+    }
+
+    .summary-sentence {
+        margin-bottom: 2rem;
+
+        font-size: 2rem;
+        font-weight: bold;
+        line-height: 3rem;
+    }
+
+    .summary-sentence span {
+        background-color: #05a1ad;
+        color: #fff;
+    }
+
+    @keyframes wave {
+        0% { 
+            transform: rotate(0deg); 
+        }
+
+        10% { 
+            transform: rotate(14deg); 
+        }
+
+        20% { 
+            transform: rotate(-8deg); 
+        }
+
+        30% { 
+            transform: rotate(14deg); 
+        }
+
+        40% { 
+            transform: rotate(-4deg); 
+        }
+
+        50% { 
+            transform: rotate(10deg); 
+        }
+
+        60% { 
+            transform: rotate(0deg); 
+        }
+
+        100% { 
+            transform: rotate(0deg); 
+        }
+    }
+
+    .contact-screen p {
+
+        margin-bottom: 1rem;
+
+        font-size: 1.2rem;
+        line-height: 1.5rem;
+    }
+
+    .contact-screen .contact-link-screen {
+        
+        display: flex;
+        gap: 1rem;
+    }
+
+    @media(max-width: 769px){
+
+        .hello-section {
+            font-size: 2rem;
+        }
+
+        .summary-sentence {
+            font-size: 1.6rem;
+        }
+
+        .summary-sentence span {
+            color: #000;
+        }
+
+        @media(max-width: 425px){
+            .contact-screen .contact-link-screen {
+                gap: .1rem;
+                flex-wrap: wrap;
+            }
+        }
+
+        @media(max-width: 375px){
+            .summary-sentence {
+                font-size: 1.4rem;
+                line-height: 2.2rem;
+            }
+
+            .contact-screen p {
+                font-size: 1rem;
+            }
+        }
+    }
+`
