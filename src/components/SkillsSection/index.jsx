6ceac9086f76c69c98a64f28ac209ed27f8658c1
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-07 08:51:24 -0300

    Adicionando novos componentes

---
+++ src/components/SkillsSection/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styled';
+
+const SkillsSection = () => {
+  return (
+    <Container>
+      SkillSection
+    </Container>
+  );
+};
+
+export default SkillsSection;
