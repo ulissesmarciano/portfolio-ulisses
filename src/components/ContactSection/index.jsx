29e9d48d55567b008872526cdfb9e62ae6e6d9c5
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-12 16:07:25 -0300

    Adicionando todos os itens da seção e folha de estilos

--- src/components/ContactSection/index.jsx
+++ src/components/ContactSection/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import { Container } from './styles';
+import LinkItem from '../LinkItem';
+
+import GithubIcon from '../../assets/icons/github-black.svg';
+import LinkedInIcon from '../../assets/icons/linkedin-black.svg';
+import DribbbleIcon from '../../assets/icons/dribbble.svg';
+
 
 const ContactSection = () => {
   return (
     <Container>
         <h3>Quer entrar em contato?</h3>
+        <p className='contact-invite'>Tem uma ideia de projeto? Eu adoraria colaborar. Envie uma mensagem. 🎣</p>
+
+        <div className='image-email-container'>
+            <picture>
+                <img className='user-image' src='https://avatars.githubusercontent.com/u/104742158?s=400&u=b793765b9ab0c8f5bec5e2c7580678cac6bce511&v=4' alt='Avatar do Ulisses' />
+            </picture>
+            <div className='name-email-section'>
+                <h1>Ulisses Marciano</h1>
+                <p>ulissesmkt2104@gmail.com</p>
+            </div>
+        </div>
+        <div className='contact-buttons-container'>
+            <LinkItem 
+                name="linkedIn"
+                variant="greetingsContactLinkIcon"
+                src={LinkedInIcon}
+            />
+            <LinkItem 
+                name="Github"
+                variant="greetingsContactLinkIcon"
+                src={GithubIcon}
+            />
+            <LinkItem 
+                name="Dribbble"
+                variant="greetingsContactLinkIcon"
+                src={DribbbleIcon}
+            />
+        </div>
     </Container>
   );
 };
--- src/components/ContactSection/styles.js
+++ src/components/ContactSection/styles.js
@@ -1,5 +1,68 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
+    margin: 0 auto;
 
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+
+    h3 {
+        font-size: 1.75rem;
+    }
+
+    .contact-invite {
+        font-size: 1.12rem;
+    }
+
+    .image-email-container {
+        display: flex;
+        align-items: center;
+        gap: 4rem;
+    }
+
+    .image-email-container .user-image {
+        height: 10rem;
+        width: 10rem;
+
+        border-radius: 100px;
+    }
+
+    .image-email-container .name-email-section h1 {
+        font-size: 1.2rem;
+    }
+
+    .image-email-container .name-email-section p {
+        font-size: 1.12rem;
+
+        padding-bottom: .3rem;
+
+        position: relative;
+        overflow: hidden;
+
+        &:after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            height: .3rem;
+            background-color: #05a1ad;
+            z-index: -1;
+            transition: height 200ms ease;
+        }
+
+        &:hover {
+            &:after {
+                height: 100%;
+
+            }
+            color: #fff;
+        }
+    }
+
+    .contact-buttons-container {
+        display: flex;
+        gap: 1.5rem;
+    }
 `
