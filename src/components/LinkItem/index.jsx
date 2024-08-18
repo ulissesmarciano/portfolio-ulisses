558e756d74cf4fb82752de93c976cff7ddc99429
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2024-08-18 12:27:13 -0300

    Enviando o parâmetro href para renderização do compomente LinkItem

--- src/components/LinkItem/index.jsx
+++ src/components/LinkItem/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Container } from './styles'
 
-const LinkItem = ({name, variant, href, src, alt}) => {
+const LinkItem = ({name, variant, href, src, alt, target}) => {
   return (
-    <Container variant={variant} href={href}>
+    <Container variant={variant} href={href} target={target}>
       <img src={true ? src : !src} alt={alt}/>
         {name}
     </Container>
