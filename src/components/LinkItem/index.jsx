0af2d84b2a4b575e83dde792dd1e093f3f62cf5e
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2024-08-24 21:11:03 -0300

    Atualização de parâmetro no link

--- src/components/LinkItem/index.jsx
+++ src/components/LinkItem/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Container } from './styles'
 
-const LinkItem = ({name, variant, href, src, alt, target}) => {
+const LinkItem = ({name, variant, href, src, alt, target, onClick}) => {
   return (
-    <Container variant={variant} href={href} target={target}>
+    <Container variant={variant} href={href} target={target} onClick={onClick}>
       <img src={true ? src : !src} alt={alt}/>
         {name}
     </Container>
