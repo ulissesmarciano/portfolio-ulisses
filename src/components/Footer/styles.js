aa494e9e6e4535ac93a1d315095add06fcc2d3d6
Ulisses <ulissesmkt2104@gmail.com>
2024-09-16 10:55:46 -0300

    Correção semântica de marcação do código

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const Container = styled.footer`
     background-color: #000;
 
     .bottom-info-section {
