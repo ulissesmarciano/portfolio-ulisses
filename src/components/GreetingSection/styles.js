66833e32dfd7f0533c16f8db6116580ce250e8f3
Ulisses <ulissesmkt2104@gmail.com>
2024-09-16 10:56:26 -0300

    Mudando orientação do GreetingSection para mobile first

--- src/components/GreetingSection/styles.js
+++ src/components/GreetingSection/styles.js
@@ -11,7 +11,7 @@ const slideUp = keyframes`
   }
 `;
 
-export const Container = styled.section`
+export const Container2 = styled.section`
 
     opacity: 0;
     transform: translateY(20px);
@@ -139,4 +139,124 @@ export const Container = styled.section`
             }
         }
     }
-`;
+`
+
+export const Container = styled.section`
+    padding-bottom: 8rem;
+
+    opacity: 0;
+    transform: translateY(20px);
+    transition: all 0.5s ease-out;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 2rem;
+
+    &.is-visible {
+        animation: ${slideUp} 0.5s forwards;
+    }
+
+    height: calc(100vh - 136.78px);
+
+    .hello-section {
+        margin-bottom: 2.2rem;
+
+        display: flex;
+        align-items: flex-start;
+        gap: 1rem;
+
+        font-size: 2rem;
+    }
+
+    .hello-section .waving-hand {
+        font-size: 2rem;
+        display: inline-block;
+        animation: wave 2s infinite;
+        transform-origin: 70% 70%;
+    }
+
+    @keyframes wave {
+        0% { 
+            transform: rotate(0deg); 
+        }
+
+        10% { 
+            transform: rotate(14deg); 
+        }
+
+        20% { 
+            transform: rotate(-8deg); 
+        }
+
+        30% { 
+            transform: rotate(14deg); 
+        }
+
+        40% { 
+            transform: rotate(-4deg); 
+        }
+
+        50% { 
+            transform: rotate(10deg); 
+        }
+
+        60% { 
+            transform: rotate(0deg); 
+        }
+
+        100% { 
+            transform: rotate(0deg); 
+        }
+    }
+
+    .summary-sentence {
+
+        font-size: 1.4rem;
+        line-height: 2.2rem;
+        font-weight: bold;
+    }
+
+    .summary-sentence span {
+        background-color: #05a1ad;
+        color: #fff;
+    }
+
+    .contact-screen p {
+        margin-bottom: 1rem;
+    }
+
+    .contact-screen .contact-link-screen {
+        display: flex;
+        gap: .1rem;
+        flex-wrap: wrap;
+    }
+
+    @media(min-width: 375px){
+        height: calc(100vh - 88.38px);
+        .summary-sentence {
+            font-size: 1.6rem;
+            line-height: 3rem;
+        }
+
+        .contact-screen p {
+            font-size: 1.2rem;
+            line-height: 1.5rem;
+        }
+
+        .contact-screen .contact-link-screen {
+            gap: 1rem;
+        }
+    }
+
+    @media(min-width: 768px){
+        .hello-section {
+            font-size: 2.625rem;
+        }
+
+        .summary-sentence {
+            font-size: 2rem;
+        }
+    }
+`
