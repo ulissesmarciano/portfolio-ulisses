2367ddfe5148db5d71dc0acc4856b33c4e51963e
Ulisses <ulissesmkt2104@gmail.com>
2024-10-23 22:43:33 -0300

    Atualização: Correção de bug na renderização do darkmode

--- src/style/global.js
+++ src/style/global.js
@@ -18,37 +18,3 @@ export const Globalstyle = createGlobalStyle`
     }
 `
 
-export const GlobalStyles = styled.createGlobalStyle`
-  body {
-    background-color: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    transition: all 0.25s linear;
-  }
-`;
-
-export const ToggleButton = styled.button`
-  background: ${({ theme }) => theme.toggleBorder};
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
-  border-radius: 30px;
-  cursor: pointer;
-  font-size: 0.8rem;
-  padding: 0.6rem;
-  margin: 20px;
-  outline: none;
-  position: relative;
-  width: 50px;
-  height: 25px;
-  display: flex;
-  align-items: center;
-  justify-content: ${({ theme }) => (theme.body === '#FFF' ? 'flex-start' : 'flex-end')};
-
-  &:after {
-    content: '';
-    width: 18px;
-    height: 18px;
-    background: ${({ theme }) => theme.text};
-    border-radius: 50%;
-    transition: 0.25s ease;
-    display: block;
-  }
-`;
