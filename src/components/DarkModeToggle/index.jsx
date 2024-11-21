02fe83b768ba81ff0bd359dbb3dd4fd7793ecd4a
Ulisses <ulissesmkt2104@gmail.com>
2024-11-20 23:09:27 -0300

    Criação: Criando toggle que muda o modo da página para claro ou escuro

---
+++ src/components/DarkModeToggle/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from './styles';
+
+const DarkModeToggle = ({ toggleTheme }) => {
+    const handleChange = (event) => {
+        toggleTheme();
+    };
+
+    return (
+        <Container>
+            <input
+                id="toggle"
+                type="checkbox"
+                className="toggle"
+                onChange={handleChange}
+            />
+            <label htmlFor="toggle"></label>
+        </Container>
+    );
+};
+
+export default DarkModeToggle;
