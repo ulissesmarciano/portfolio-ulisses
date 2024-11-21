d67f75f897f52dac125c148698556b6a9da453e5
Ulisses <ulissesmkt2104@gmail.com>
2024-11-20 23:11:01 -0300

    Atualização: Implementando o tema para mudança geral no estilo da página

--- src/style/global.js
+++ src/style/global.js
@@ -2,19 +2,24 @@ import { createGlobalStyle } from 'styled-components';
 
 
 export const Globalstyle = createGlobalStyle`
-    *{
+    * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: Roboto;
+        font-family: Roboto, sans-serif;
         list-style: none;
         scroll-behavior: smooth;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme?.background};
+        color: ${({ theme }) => theme?.text};
+        transition: background-color 0.3s ease, color 0.3s ease;
     }
 
     a {
         text-decoration: none;
         cursor: pointer;
-        color: #000;
     }
 `
 
