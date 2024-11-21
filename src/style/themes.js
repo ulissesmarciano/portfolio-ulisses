b5d979a8f4a59ce8c97de52b46c048d794908b48
Ulisses <ulissesmkt2104@gmail.com>
2024-11-20 23:08:23 -0300

    Criação: Criando constantes de tema escuro e claro

---
+++ src/style/themes.js
@@ -0,0 +1,23 @@
+export const lightTheme = {
+    background: '#FFFFFF',
+    text: '#000000',
+    hamburguerBgColor: '#000',
+    border: '#000',
+    linkBackgroundColor: '#000',
+    secondaryLinkTextColor: '#fff',
+    imageFilter: 'none',
+    scrollbarBorder: '#c1c1c1',
+    scrollbarBackground: '#fff'
+};
+
+export const darkTheme = {
+    background: '#121212',
+    text: '#FFFFFF',
+    hamburguerBgColor: '#fff',
+    border: '#fff',
+    linkBackgroundColor: '#fff',
+    secondaryLinkTextColor: '#000',
+    imageFilter: 'invert(100%)',
+    scrollbarBorder: '#c1c1c1',
+    scrollbarBackground: '#121212'
+};
