51594aa90dcf8199fb7d6b0a41d8825ec6fdb34f
Ulisses <ulissesmkt2104@gmail.com>
2024-11-20 23:14:23 -0300

    Atualização: Adicionando parâmetros para o funcionamento do componente toggle com inserção do parâmetro direto no componente importado Header

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Container } from './styles'
-import Header from '../components/Header'
-import AboutMeSection from '../components/AboutMeSection'
-import GreetingsSection from '../components/GreetingSection'
-import SkillsSection from '../components/SkillsSection'
-import ProjectsSection from '../components/ProjectsSection'
-import ContactSection from '../components/ContactSection'
-import Footer from '../components/Footer'
+import React from 'react';
+import { Container } from './styles';
+import Header from '../components/Header';
+import AboutMeSection from '../components/AboutMeSection';
+import GreetingsSection from '../components/GreetingSection';
+import SkillsSection from '../components/SkillsSection';
+import ProjectsSection from '../components/ProjectsSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
 
-const PortfolioSection = () => {
+const PortfolioSection = ({ toggleTheme }) => {
   return (
     <Container>
-      <Header />
+      <Header toggleTheme={toggleTheme} />
       <main className='main-body'>
         <GreetingsSection />
         <AboutMeSection />
@@ -21,7 +21,7 @@ const PortfolioSection = () => {
       </main>
       <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default PortfolioSection
+export default PortfolioSection;
