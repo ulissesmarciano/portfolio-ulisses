15afe33c19e54036faea54868e549051a9e8a982
Ulisses <ulissesmkt2104@gmail.com>
2024-11-20 23:12:25 -0300

    Atualização: Adicionando hooks para renderizar o tema em todo o projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import PortfolioSection from "./page";
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Globalstyle } from './style/global';
+import { lightTheme, darkTheme } from './style/themes';
+import PortfolioSection from './page/index';
 
-function App() {
-  return <PortfolioSection />;
-}
+const App = () => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        setTheme(savedTheme);
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+    };
+
+    return (
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <Globalstyle />
+            <PortfolioSection toggleTheme={toggleTheme} />
+        </ThemeProvider>
+    );
+};
 
 export default App;
