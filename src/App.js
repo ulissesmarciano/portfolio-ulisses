7916f27ce7973a984d0ff58b5c06652b0b206703
Ulisses <ulissesmkt2104@gmail.com>
2024-12-11 15:47:48 -0300

    Atualização: Adicionando nova função e guardando parametros de preferencia do usuário.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,31 @@ import PortfolioSection from './page/index';
 
 const App = () => {
     const [theme, setTheme] = useState('dark');
+    const [isSun, setIsSun] = useState(true);
 
     useEffect(() => {
         const savedTheme = localStorage.getItem('theme') || 'dark';
+        const savedIsSun = JSON.parse(localStorage.getItem('isSun') || 'true');
+
         setTheme(savedTheme);
+        setIsSun(savedIsSun);
     }, []);
 
     const toggleTheme = () => {
         const newTheme = theme === 'dark' ? 'light' : 'dark';
+        const newIsSun = !isSun;
+
         setTheme(newTheme);
+        setIsSun(newIsSun);
+
         localStorage.setItem('theme', newTheme);
+        localStorage.setItem('isSun', JSON.stringify(newIsSun));
     };
 
     return (
         <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
             <Globalstyle />
-            <PortfolioSection toggleTheme={toggleTheme} />
+            <PortfolioSection toggleTheme={toggleTheme} isSun={isSun} />
         </ThemeProvider>
     );
 };
