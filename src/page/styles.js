15521ade0c4b9130fea34367e49dc19b171a5c4d
Ulisses <ulissesmkt2104@gmail.com>
2024-12-10 23:31:10 -0300

    Atualização: correção de padding da página

--- src/page/styles.js
+++ src/page/styles.js
@@ -3,18 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     .main-body {
         margin: 0 auto;
+        padding: 0 1rem;
         max-width: 64rem;
-    }
-    
-    @media (max-width: 1200px){
-        .main-body {
-            padding: 0 2rem;
-        }
-    }
-
-    @media (max-width: 320px) {
-        .main-body {
-            padding: 0 1rem;
-        }
     }
 `
