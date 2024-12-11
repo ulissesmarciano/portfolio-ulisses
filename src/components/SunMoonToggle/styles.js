8f6edbfaaf89f15d83d98b3ce0aade384a34e384
Ulisses <ulissesmkt2104@gmail.com>
2024-12-11 15:39:42 -0300

    Criação: Novo toggle/button que altera o svg de sol para lua ao clicar para alterar o dark/light mode

---
+++ src/components/SunMoonToggle/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const IconContainer = styled.div`
+    position: absolute;
+
+    bottom: -4rem;
+    left: 50%;
+    transform: translateX(-100%);
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
+
+  svg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transform: rotate(360deg);
+    transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+
+    &.visible {
+      opacity: 1;
+      transform: rotate(0deg);
+      pointer-events: auto;
+    }
+
+    &.hidden {
+      pointer-events: none;
+    }
+  }
+`;
