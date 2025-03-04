eabc17c41fe55ca9e4dafc67505d3e863c720810
Ulisses <ulissesmarcianodev@gmail.com>
2025-03-03 22:56:45 -0300

    fix: retirada de estilo global duplicado no projeto

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { Globalstyle } from './style/global';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Globalstyle />
     <App />
   </React.StrictMode>
 );
